21b125819d6db3785638ee2ed891e344eb059101
FranMontoro <montoro.024@gmail.com>
2018-03-12 12:59:30 +0100

    login funciona, hacia dashboard

--- admin/login/index.php
+++ admin/login/index.php
@@ -4,12 +4,9 @@ include('config.php');
 
 $email = $_POST['email'];
 $password = md5($_POST['password']);
-//$email = "usuario@usuario.es";
-//$password = md5("usuario");
 
-$query = "SELECT * FROM usuario WHERE email='$email' AND password='$password'";
-$result = mysqli_query($mysqli, $query);
-$count = mysqli_num_rows($result);
+$query = mysqli_query($mysqli,"SELECT email, password FROM usuario WHERE (email='$email') AND (password='$password')");
+$count = mysqli_num_rows($query);
 
    if($count >=1){
        $_SESSION['canAccess'] = true;
